b003766ed1035d8d470c82c79f6d0ab4e2e237d8
Adem <ado.designs@outlook.de>
2025-06-07 23:52:59 +0200

    feat(reaction): measure and display reaction time

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let startTime;
+
 function init() {
     getCircle();
 }
@@ -9,4 +11,12 @@ function moveCircle(mo) {
     mo.style.left = `${Math.random() * (window.innerWidth - 50)}px`; // random x coordinate
     mo.style.top = `${Math.random() * (window.innerHeight - 50)}px`; // random y coordiante
     mo.style.background = `hsl(${Math.random() * 360}, 100%, 60%)`; // random color
+    startTime = Date.now();
 }
+function measureTime() {
+    const m = document.getElementById('message');
+    const reactionTime = Date.now() - startTime;
+    m.innerHTML = `Time: ${(reactionTime / 1000).toFixed(2)} Seconds`;
+    getCircle();
+}
+// function 
